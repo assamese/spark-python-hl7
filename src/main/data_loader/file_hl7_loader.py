bb62f89c1aaf67dc100cbec946b1901a8fbd5413
assamese <assamese@hotmail.com>
2020-06-01 23:29:28 -0400

    refactored

--- src/main/data_loader/file_hl7_loader.py
+++ src/main/data_loader/file_hl7_loader.py
@@ -2,15 +2,21 @@ from pathlib import Path
 
 class HL7_Loader:
     @staticmethod
-    def load_hl7_lines(folder_name):
+    def load_hl7_messages(folder_name):
         source_dir = Path(folder_name)
         files = source_dir.glob('*.txt')
         for file in files:
             with file.open('r') as file_handle:
+                hl7_message = []
                 for line in file_handle:
-                    yield line
+                    hl7_message.append(line)
+            yield hl7_message
+
 
 if __name__ == "__main__":
-    folder_name = '../hl7-data/'
-    for hl7_line in HL7_Loader.load_hl7_lines(folder_name):
-        print (hl7_line)
+    folder_name = '/home/assamese/work/python-projects/spark-python-hl7/hl7-data/'
+    for message in HL7_Loader.load_hl7_messages(folder_name):
+        print("message:")
+        for line in message:
+            print(line)
+
