eae69a67e17ea4673fe073e10cbeeafa9305d2dc
assamese <assamese@hotmail.com>
2020-06-04 14:45:14 -0400

    added Covid related ..1

--- spark_hl7.py
+++ spark_hl7.py
@@ -20,8 +20,9 @@ class SparkApp:
         hl7_pair_rdd = sparkContext.wholeTextFiles(folder_name + "sample*.txt")
         #print(hl7_pair_rdd.take(1))
         hl7_rdd = hl7_pair_rdd.map(lambda x: x[1])
-        print(hl7_rdd.take(1))
+        #print(hl7_rdd.take(10))
 
+        # DO NOT COMMENT THIS LINE
         dummySparkSession = SparkSession(sparkContext) # see this: https://stackoverflow.com/questions/32788387/pipelinedrdd-object-has-no-attribute-todf-in-pyspark
 
         df_hl7 = hl7_rdd.map(lambda x: (HL7_Parser.get_patient_id_from_RDD(x), HL7_Parser.get_message_type_from_RDD(x), x, datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))).toDF(["patient_id", "message_type", "hl7_message", "timestamp"])
